3abbfea23e91d04e6ed2f8134a910297c534d62a
k0344000 <154581692+k0344000@users.noreply.github.com>
2024-01-26 14:29:18 +0900

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: "furimas#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :furimas do
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
