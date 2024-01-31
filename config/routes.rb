bb99e38bf338d2e567e293308f768f3e295dc730
k0344000 <154581692+k0344000@users.noreply.github.com>
2024-01-31 20:12:36 +0900

    管理機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "furimas#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :furimas do
+    resources :orders, only: [:index]
   end
   # Defines the root path route ("/")
   
