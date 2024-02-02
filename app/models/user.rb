bc6773b238c8ec04b52383d8d474c98cfaef0f61
k0344000 <154581692+k0344000@users.noreply.github.com>
2024-02-02 14:31:06 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :orders
 
          validates :nickname, presence: true
-         validates :email, presence: true
-         validates :password, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :birthday, presence: true
 
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } 
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } 
+       
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } 
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } 
+       
+        
+         validates :birthday, presence: true
 end
