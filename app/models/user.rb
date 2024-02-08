54b5d206f6fc358383945fdb0b54e9a16964a2d2
k0344000 <154581692+k0344000@users.noreply.github.com>
2024-02-08 20:14:59 +0900

    商品一覧表示機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
+  
          validates :nickname, presence: true
 
          validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])[!-~]+\z/ }
