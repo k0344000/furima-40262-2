610f316637c4aecc74c74a37da4e46a6dd12ae03
k0344000 <154581692+k0344000@users.noreply.github.com>
2024-02-10 18:50:34 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show ]
+  before_action :set_item, only: [:show, :destroy ]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -37,7 +37,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
     redirect_to root_path
   end
